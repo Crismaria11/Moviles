ce89b82ca95c818b623923326e4d4ad548c13dda
Cristina B <bau161260@uvg.edu.gt>
2019-01-23 07:29:06 -0600

    Lab#3

---
+++ Laboratorio#3/app/src/main/java/com/example/cristinabautista/lab3/Lab3.kt
@@ -0,0 +1,12 @@
+package com.example.cristinabautista.lab3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Lab3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab3)
+    }
+}
